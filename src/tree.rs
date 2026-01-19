bf3ac2fefa7267900fe2b3c1635477419d948afd
Migo <saka90030@gmail.com>
2026-01-19 23:39:36 +0400

    root level hash

--- src/tree.rs
+++ src/tree.rs
@@ -54,35 +54,65 @@ impl InternalNode {
 }
 
 impl Root {
+    pub fn new(max_count: u64) -> Self {
+        Self {
+            hash: [0; 32],
+            children: vec![],
+            max_count,
+        }
+    }
+
     pub fn hash(&self) -> &[u8; 32] {
         &self.hash
     }
 
-    pub fn insert(&mut self, key: String, value: String) {
-        // find children which can host the key
-        let mut hasher = sha2::Sha256::new();
-        hasher.update(value);
-        let hash = hasher.finalize();
+    fn xor_hash(&mut self, child_hash: [u8; 32]) {
+        for i in 0..32 {
+            self.hash[i] ^= child_hash[i]
+        }
+    }
 
-        match self.children.binary_search_by_key(&&key, |a| &a.max_key) {
-            Ok(found) => todo!(),
-            Err(not_found) => {
-                match (
-                    self.children.get(not_found.saturating_sub(1)), // ! is it okay to use sat sub?
-                    self.children.get(not_found),
-                ) {
-                    (None, None) => {
-                        let mut int_node = InternalNode::new();
-                        int_node.add_leaf(Leaf {
-                            key,
-                            hash: hash.into(),
-                        });
-                    }
-                    (None, Some(_)) => todo!(),
-                    (Some(_), None) => todo!(),
-                    (Some(_), Some(_)) => todo!(),
-                }
-            }
+    pub fn insert(&mut self, key: String, value: String) {
+        // Create Leaf
+        let mut hasher = sha2::Sha256::new();
+        hasher.update(value.as_bytes());
+        let hash: [u8; 32] = hasher.finalize().into();
+        let leaf = Leaf { key, hash };
+
+        // In case of first insertion
+        if self.children.is_empty() {
+            // Case: Empty Tree
+            let mut new_node = InternalNode::new();
+            new_node.add_leaf(leaf);
+            self.xor_hash(new_node.hash); // Update root hash
+            self.children.push(new_node);
+            return;
+        }
+
+        // Locate Correct Child Node
+        let child_index = self
+            .children
+            .partition_point(|node| &node.max_key < &leaf.key);
+
+        let target_index = if child_index < self.children.len() {
+            // Case: Key fits in an existing range
+            child_index
+        } else {
+            // Case: Key is larger than all ranges, child_index is equal to lenth of vector in this case.
+            child_index - 1
+        };
+
+        // Cancel out old hash
+        self.xor_hash(self.children[target_index].hash);
+
+        self.children[target_index].add_leaf(leaf);
+
+        // Reapply hash of the children
+        self.xor_hash(self.children[target_index].hash);
+
+        // 5. Handle Node Splitting
+        if self.children[target_index].children.len() as u64 > self.max_count {
+            // TODO: Split the node
         }
     }
 }
@@ -124,4 +154,43 @@ mod test {
 
     #[test]
     fn addleaf_creates_deterministic_hash() {}
+
+    #[test]
+    fn insert_creates_nodes_correctly() {
+        let mut root = Root::new(10);
+        root.insert("key1".to_string(), "value1".to_string());
+
+        assert_eq!(root.children.len(), 1);
+        assert_eq!(root.children[0].children.len(), 1);
+        assert_eq!(root.children[0].children[0].key, "key1");
+        assert_eq!(root.children[0].max_key, "key1");
+
+        root.insert("key3".to_string(), "value3".to_string());
+        assert_eq!(root.children.len(), 1);
+        assert_eq!(root.children[0].children.len(), 2);
+        assert_eq!(root.children[0].children[1].key, "key3");
+        assert_eq!(root.children[0].max_key, "key3"); // max_key is updated
+
+        root.insert("key2".to_string(), "value2".to_string());
+        assert_eq!(root.children.len(), 1);
+        assert_eq!(root.children[0].children.len(), 3);
+        assert_eq!(root.children[0].children[0].key, "key1");
+        assert_eq!(root.children[0].children[1].key, "key2");
+        assert_eq!(root.children[0].children[2].key, "key3");
+        assert_eq!(root.children[0].max_key, "key3"); // max_key is still key3
+    }
+
+    #[test]
+    fn insert_updates_root_hash() {
+        let mut root = Root::new(10);
+        let initial_hash = root.hash().clone();
+
+        root.insert("key1".to_string(), "value1".to_string());
+        let hash_after_1 = root.hash().clone();
+        assert_ne!(initial_hash, hash_after_1);
+
+        root.insert("key2".to_string(), "value2".to_string());
+        let hash_after_2 = root.hash().clone();
+        assert_ne!(hash_after_1, hash_after_2);
+    }
 }
